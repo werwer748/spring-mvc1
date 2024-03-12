9a45bfca55e0b5f5a451e3512f46e6254f40a4a5
hugo <werwer748@naver.com>
2024-03-12 22:10:19 +0900

    MVC1 완

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,42 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    // 실무에서는 HashMap X => ConcurrentHashMap O, long X => AtomicLong O
+    private static final Map<Long, Item> store = new HashMap<>(); // static
+
+    private static long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) { // 원래는 Dto로 쓰는게 맞다.
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
